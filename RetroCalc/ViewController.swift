6e7e8507aca4d74b4c5a062a2c196b5aad1cfc50
Zack Falgout <zfalgout@gmail.com>
2017-05-25 06:26:18 -0500

    Added sounds and began implementing mathematical operations

--- RetroCalc/ViewController.swift
+++ RetroCalc/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var btnSound: AVAudioPlayer!
 
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var runningNumber = ""
+    
+    enum Operation: String //Lecture stopped here --- 6:46 in.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        } catch let e as NSError {
+            print("Error playing button sound: \(e.debugDescription)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func numberPressed(sender: UIButton){
+        playSound()
+        
+        runningNumber += "\(sender.tag)"
+        outputLbl.text = runningNumber
+        
+    }
+    
+    func playSound(){
+        if btnSound.isPlaying{
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+    
+    
 }
 
